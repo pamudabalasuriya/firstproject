552f9df9eb539d14d0b6aacd538c5c6ea173621e
pamuda1995@gmail.com <pamuda1995@gmail.com>
2025-02-11 15:59:45 +0530

    day 12-Final

---
+++ typesc/type.ts
@@ -0,0 +1,16 @@
+let x:number = 3;
+x = 4;
+
+let markArray: Array<Number> = [1,2,3];
+markArray.push("scdas")
+
+
+class Student {
+    id: number;
+    constructor(id: number, name: string){
+
+    }
+    
+};
+
+let student1:Student = new Student(1,"qwesd")
